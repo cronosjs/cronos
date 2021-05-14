857efa7e68c155e8b97506f8aa809d43c3cedae2
Renox <realrenox@gmail.com>
2021-05-14 17:44:57 +0200

    Added .env and added .idea to .gitignore

--- cronos.js
+++ cronos.js
@@ -1,12 +1,15 @@
+require("dotenv").config({path: __dirname + "/.env"})
 const { Client, Collection } = require("discord.js");
 const client = new Client({ disableMentions: "everyone" });
-const { loadCommands } = require("./loadcmds");
+// const { loadCommands } = require("./loadcmds");
 
-require("./loadevents")(client);
+// require("./loadevents")(client);
 
-client.login(TOKEN);
+client.login(process.env.main_token).then((x) => //X is the Token
+    console.log(`Successfully logged in as: ${client.user.username}#${client.user.discriminator}`)
+);
 client.commands = new Collection();
-client.prefix = PREFIX;
+// client.prefix = PREFIX;
 client.queue = new Map();
 
-loadCommands(client);
+// loadCommands(client);
