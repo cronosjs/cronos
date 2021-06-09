d2f357c63ca4e154f80d5864026ea9515bcc41ee
Ayman Zaakouki Boukraa <thettayman@gmail.com>
2021-06-09 02:31:16 +0200

    delete xp and data

---
+++ src/events/guildDelete.js
@@ -0,0 +1,7 @@
+const guildDoc = require("../models/guild");
+
+module.exports = async (guild, client) => {
+  await client.xp.deleteGuild(guild.id);
+
+   await guildDoc.findOneAndDelete({ Guild: guild.id });
+};
