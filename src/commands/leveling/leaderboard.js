1ad4a0e9eec08d81d554e4147a8664ae69ca9d5b
Renox <renoxgit@gmail.com>
2021-06-09 16:47:55 +0200

    Rearranged output of leaderboard

--- src/commands/leveling/leaderboard.js
+++ src/commands/leveling/leaderboard.js
@@ -13,10 +13,10 @@ module.exports = {
 
     if (!top) return message.reply("Cri cri... No one has xp in this server");
 
-    let lbEmbed = new MessageEmbed().setTitle(`${message.guild}'s leaderoard`);
+    let lbEmbed = new MessageEmbed().setTitle(`${message.guild}'s leaderboard`);
 
     for (let i = 0; i < top.length; i++) {
-      lbEmbed.addField(`**${i + 1}:** ${message.guild.members.cache.get(top[i][0]).user.username}`, `**${top[i][1].xp}** (lvl. ${top[i][1].level})` );
+      lbEmbed.addField(`**${i + 1}:** ${message.guild.members.cache.get(top[i][0]).user.username}`, `Level: **${top[i][1].level}** (XP: ${top[i][1].xp})` );
     }
 
     return message.channel.send(lbEmbed);
