e359fa7092c3ae7efeb3a6fda9ef22acfc3158af
Ayman Zaakouki Boukraa <thettayman@gmail.com>
2021-06-20 14:49:48 +0200

    added db to Tic Tac Toe

--- src/models/guild.js
+++ src/models/guild.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
 
-const guild = new mongoose.Schema({
-  Guild: String,
-  Prefix: String,
-  wcChannel: String,
-  lvlChannel: String,
-  logChannel: String,
-});
+const guild = new mongoose.Schema(
+  {
+    Guild: String,
+    Prefix: String,
+    wcChannel: String,
+    lvlChannel: String,
+    logChannel: String,
+  },
+  { versionKey: false }
+);
 
 module.exports = mongoose.model("guild", guild);
--- src/models/player.js
+++ src/models/player.js
@@ -1,24 +1,27 @@
 const mongoose = require("mongoose");
 
-const player = new mongoose.Schema({
-  _id: String,
-  Play: Number,
-  Win: Number,
-  Lose: Number,
+const player = new mongoose.Schema(
+  {
+    _id: String,
+    play: Number,
+    win: Number,
+    lose: Number,
 
-  // Blackjack stats
-  blackJack: {
-    play: Number,
-    win: Number,
-    lose: Number,
+    // Blackjack stats
+    blackJack: {
+      play: Number,
+      win: Number,
+      lose: Number,
+    },
+
+    // Tic Tac Toe stats
+    ticTacToe: {
+      play: Number,
+      win: Number,
+      lose: Number,
+    },
   },
-  
-  // Tic Tac Toe stats
-  ticTacToe: {
-    play: Number,
-    win: Number,
-    lose: Number,
-  },
-});
+  { versionKey: false }
+);
 
 module.exports = mongoose.model("player", player);
